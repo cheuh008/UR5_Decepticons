7e072689bcb61a45d74cb12550efd07c832fbf86
Harry <cheuh008@gmail.com>
2025-02-25 10:32:13 +0000

    Async?

--- examples/test.py
+++ examples/test.py
@@ -6,7 +6,7 @@ import argparse
 import math 
 import cv2
 import asyncio
-
+import threading
 
 # Add the directory containing robotiq_preamble.py to the Python search path
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -15,13 +15,12 @@ sys.path.append(os.path.join(current_dir, 'robotiq'))
 from utils.UR_Functions import URfunctions as URControl
 from robotiq.robotiq_gripper import RobotiqGripper
 
-
 HOST = "192.168.0.2"
 PORT = 30003
 
 async def main():
     robot = URControl(ip="192.168.0.2", port=30003)
-    gripper=RobotiqGripper()
+    gripper = RobotiqGripper()
     gripper.connect("192.168.0.2", 63352)
 
     new_home = [1.3348536491394043, -1.6144734821715296, 1.5730488936053675, -1.5615404548919578, -1.5539191404925745, -0.2927349249469202]
@@ -33,42 +32,41 @@ async def main():
     end_plate_intermediate_0 = [1.6996548175811768, -1.1775085192969819, 1.7128971258746546, -2.111119409600729, -1.5870631376849573, -0.2889450232135218]
     end_plate_0 = [1.6991523504257202, -1.1309567254832764, 1.7128971258746546, -2.1226536236205042, -1.587397877370016, -0.28891116777528936]
 
-  
     def goto(pos):
         robot.move_joint_list(pos, 0.7, 0.5, 0.02)
 
     def open_gripper():
-        gripper.move(0,125,125) # Open Gripper
+        gripper.move(0, 125, 125)  # Open Gripper
 
     def close_gripper():
-        gripper.move(255,125,125) # Close Gripper
+        gripper.move(255, 125, 125)  # Close Gripper
 
-    # async def open_camera():
-    #     cam = cv2.VideoCapture(0)
-    #     cv2.namedWindow("test")
-    #     img_counter = 0
-    #     while True:
-    #         ret, frame = cam.read()
-    #         if not ret:
-    #             print("failed to grab frame")
-    #             break
-    #         cv2.imshow("test", frame)
+    async def open_camera():
+        cam = cv2.VideoCapture(0)
+        cv2.namedWindow("test")
+        img_counter = 0
+        while True:
+            ret, frame = cam.read()
+            if not ret:
+                print("failed to grab frame")
+                break
+            cv2.imshow("test", frame)
         
-    #         k = cv2.waitKey(1)
-    #         if k%256 == 27:
-    #         #ESC pressed
-    #             print("Escape hit , closing...")
-    #             break
+            k = cv2.waitKey(1)
+            if k % 256 == 27:
+                # ESC pressed
+                print("Escape hit, closing...")
+                break
 
-    #         elif k%256 ==32:
-    #         #SPACE pressed
-    #             img_name = "opencv_frame_{}.png".format(img_counter)
-    #             cv2.imwrite(img_name, frame)
-    #             print("{} written!".format(img_name))
-    #             img_counter +=1
-    #         cam.release
-    #     cv2.destroyAllWindows()   
- 
+            elif k % 256 == 32:
+                # SPACE pressed
+                img_name = "opencv_frame_{}.png".format(img_counter)
+                cv2.imwrite(img_name, frame)
+                print("{} written!".format(img_name))
+                img_counter += 1
+        cam.release()
+        cv2.destroyAllWindows()
+
     goto(new_home)
     open_gripper()
 
@@ -83,14 +81,16 @@ async def main():
     goto(stirrer_position)
     open_gripper()
     goto(new_home)
-    time.sleep(5)
+    await asyncio.sleep(5)
     goto(stirrer_position)
     close_gripper()
     goto(new_home)
 
     goto(camera_position)
-    # await open_camera()
-    time.sleep(5)
+    # Run camera in a separate thread
+    camera_thread = threading.Thread(target=asyncio.run, args=(open_camera(),))
+    camera_thread.start()
+    await asyncio.sleep(5)
     # add an if or else statement depending on camera run to go to end or stirrer
     # also add if stirred 3 times then go to failed section
 
@@ -101,9 +101,9 @@ async def main():
     goto(new_home)
 
 def degreestorad(list):
-     for i in range(6):
-          list[i]=list[i]*(math.pi/180)
-     return(list)
+    for i in range(6):
+        list[i] = list[i] * (math.pi / 180)
+    return list
 
 if __name__ == '__main__':
-     main()
+    asyncio.run(main())
