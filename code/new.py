83d16001d47ad3a93520d7ceeb6c00a33ef130f2
Harry Wing Fung Cheung <60906789+cheuh008@users.noreply.github.com>
2025-03-11 14:10:02 +0000

    Updated Workflow, Added Debug, removed redudant code, Updated Positions (#20)

--- code/new.py
+++ code/new.py
@@ -20,36 +20,34 @@ from typing import Optional
 # region Constants
 # =============================================================================
 
-file_path = os.path.join(current_dir, 'data', 'positions.json')
-with open(file_path, "r") as json_file:
-    POSITIONS = json.load(json_file)
-
 HOST = "192.168.0.2"
 PORT = 30003
-MAX_ROUNDS = 2
-SLEEP_TIME = 2
 ITERATIONS = 4
 
 Rex = URControl(ip=HOST, port=PORT)
 gripper = RobotiqGripper()
 gripper.connect(HOST, 63352)
 
+file_path = os.path.join(current_dir, 'data', 'positions.json')
+with open(file_path, "r") as json_file:
+    POSITIONS = json.load(json_file)
+
 # =============================================================================
 # region Helper Function
 # =============================================================================
 
-def move_to(key: str, i: Optional[int] = None) -> None:
+def move_to(key: str, i = None):
     """Moves Robot Arm (Rex) to the specified position."""
-    pos = POSITIONS[key][i] if i else POSITIONS[key]
-    Rex.move_joint_list(pos, 0.7, 0.5, 0.05)
+    pos = POSITIONS[key] if i is None else POSITIONS[key][i]
+    Rex.move_joint_list(pos, 1, 0.75, 0.05)
     print(f"Moving to {key}"  + (f" (for vial: {i})" if i else ""))
 
-def grab() -> None:
+def grab():
     """Close the gripper """
     print("Closing Gripper, Grabbing Vial")
     gripper.move(255, 125, 125)
 
-def ungrab() -> None:
+def ungrab():
     """Opens the gripper """
     print("Opening Gripper, Releasing Vial")
     gripper.move(0, 125, 125)
@@ -58,87 +56,70 @@ def ungrab() -> None:
 # region Colour Checker
 # =============================================================================
 
-def check() -> bool:
-    """ 
-    Executes a workflow loop to check for color change 
-    Returns: 
-        bool: True if colour change detected, else False
+def check(slp = 5, tries= 2, colour_change = False, round = 0,):
+    """ Colour Change detector Loop 
+
+    Parameters:
+        tries (int): The number of tries (iterations) the loop will run. Default is 2.
+
+        slp (int or float): The number of seconds to wait (sleep) between each check. Default is 5 seconds.
+        colour_change (bool): A flag indicating whether a color change has occurred. 
+                            The loop will stop if this is True. Default is False.
+        round (int): Starting round / iterations. Default is 2.
+
+    Returns: None
     """
-
-    # Taking Vial to Stirer (Shaker)
-    move_to('stir_interm')
-    move_to('stirer')
-    ungrab()
-
-    # Waiting 
-    move_to('home')
-    print(f"Wating for {SLEEP_TIME} seconds")
-    time.sleep(SLEEP_TIME)
-    move_to('stirer')
-    grab()
     
-    # Taking Vial to Camera
-    move_to('home')
-    move_to('camera')
-    process_image()
-
-    return False # Placeholder, replace with actual color change detection logic
-
-# =============================================================================
-# region Iter Workflow
-# =============================================================================
-
-def workflow(i):
-    """ 
-    Executes the workflow for a single vial
-    
-    Args: 
-        i (int): The iteration/vial number.
-    """ 
-
-    # prep, opening gripper for pick up
-    ungrab() 
-    move_to('home')
-    move_to('pickup', i)
-    grab()
-
-    move_to('VP_interm')
-    move_to('home')
-
-    rounds = 0 # time out tracker
-    colour_change = False 
-    colour_change = check()
-    
-    while not colour_change and rounds <= MAX_ROUNDS:
-        check()
-        rounds += 1
+    while not colour_change and round <= tries:
+        # Vial Drop Off
+        move_to('stir_interm')
+        move_to('stirer')
+        ungrab()
+        # Vial Waititing
+        move_to('stir_interm')
+        time.sleep(slp)
+        print(f"Wating for {slp} seconds")
+        # Vial Pickup
+        move_to('stirer')
+        grab()
+        move_to('stir_interm')
+        # Camera AI Logic 
+        move_to('camera')
+        process_image()
+        round += 1     
+        # detector loop break
         if colour_change:
             print("Yay")
-        elif rounds == MAX_ROUNDS:
-            print("Max Round Timeout, Ending Loop")
+            return
         else:
-            print(f"No Colour Change Detected, Attempt {round}/{MAX_ROUNDS}")
-    
-    move_to('home')
-    move_to('end_interm', i)
-    move_to('end', i)
-    ungrab()
-    move_to('home')
-    print("Workflow End")
+            print(f"No Colour Change Detected, Attempt {round}/{tries}")
 
-# =============================================================================
-# region Main Iter 
-# =============================================================================
+    print("Timeout: Max Tries. Poceeding to end")        
+    return 
 
-def main() -> None:
-    """Main execution function."""
-    
+def main():
+    """ Executes the workflow for a (i) vial(s) """ 
+
+    # i = 0 debug override default 4
     for i in range(ITERATIONS):
         print(f"Processing iteraation {i}")
-        workflow(i)
+        ungrab() 
+        move_to('start')
+        move_to('pickup', i)
+        grab()
+        move_to('start')
+
+        # Debug Overide
+        check(slp=0, tries=0)    
+
+        move_to('end_interm', i)
+        move_to('end', i)
+        ungrab()
+        move_to('home')
+        print("Workflow End")
+        #debug(i)
 
     print("All vials processed. Exiting program.")
-
 
 # =============================================================================
 # region Main
@@ -146,3 +127,29 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+    print("Workflow End")
+
+
+    # Debug    
+    # i = 0
+    # ungrab()
+    # move_to('start')
+    # move_to('pickup', i)
+    # grab()
+    # move_to('start')
+    # move_to('stir_interm')
+    # move_to('stirer')
+    # ungrab()
+    # move_to('stir_interm')
+    # # time.sleep(2)
+    # move_to('stirer')
+    # grab()
+    # move_to('stir_interm')
+    # move_to('camera')
+    # move_to('end_interm', i)
+    # move_to('end', i) 
+    # ungrab()
+    # move_to('home')
+    # print("Workflow End")
+
+
